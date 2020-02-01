17da5c0f417cebec02a362de1053d116e6583cf3
Melati <leomelati@gmail.com>
2020-02-01 20:56:56 -0300

    Add file ft_strrchr.c

--- Libft/libft.h
+++ Libft/libft.h
@@ -32,5 +32,6 @@ void    *ft_memchr(const void *str, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
 
 #endif
