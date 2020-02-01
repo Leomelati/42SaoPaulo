2154556ddd362c4439cfa4c1ea04100b1f85f150
Melati <leomelati@gmail.com>
2020-02-01 20:46:52 -0300

    Add files to test

--- Libft/libft.h
+++ Libft/libft.h
@@ -30,5 +30,7 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void    *ft_memmove(void *dest, const void *srd, size_t n);
 void    *ft_memchr(const void *str, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *str, int c);
 
 #endif
