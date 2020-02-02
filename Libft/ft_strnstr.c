047914e0708ebd42ba64adfd5fc54db415bb0806
Melati <leomelati@gmail.com>
2020-02-01 23:11:43 -0300

    Create file ft_strnstr.c (WIP)

---
+++ Libft/ft_strnstr.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmartins <lmartins@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/01 21:57:22 by lmartins          #+#    #+#             */
+/*   Updated: 2020/02/01 21:57:22 by lmartins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+char    ft_strnstr(const char *big, const char *little, size_t len)
+{
+
+
+    
+}
