81549cddecdde0d93da3b14c8ec072fbf2bad00b
Leonardo Martins melati <lmartins@c1r7p3.42sp.org.br>
2020-03-04 11:40:39 -0300

    Libft Finished 115/100

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: lmartins <lmartins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 02:14:58 by lmartins          #+#    #+#             */
-/*   Updated: 2020/03/02 11:40:08 by lmartins         ###   ########.fr       */
+/*   Updated: 2020/03/04 11:23:05 by lmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define BUFFER_SIZE 5
 
 size_t	get_len(const char *str)
 {
@@ -29,11 +28,7 @@ int		index_caract(char *buffer, int c)
 
 	i = 0;
 	while (buffer[i] != (char)c && buffer[i])
-	{
-		// // printf(" %c ", buffer[i]);
 		i++;
-	}
-	// // printf("\n");
 	if (buffer[i] != (char)c)
 		return (-1);
 	return (i);
@@ -63,50 +58,23 @@ int		get_next_line(int fd, char **line)
 	static char		*str = NULL;
 	char			buffer[BUFFER_SIZE + 1];
 
-	// printf("\nStr:\n%s\n", str);
 	if (BUFFER_SIZE < 1 || !line || read(fd, buffer, 0) < 0)
 		return (-1);
 	if (str && ((i = index_caract(str, '\n')) != -1))
-	{
-		// printf("\n\n\n\n\n\n\n\n\n\nEntrou no Segundo If\n");
-		// printf("i: %d\n", i);
 		return (get_line(&str, line, i));
-	}
 	while ((ret = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		// // printf("\nEntrou no While\n");
 		buffer[ret] = '\0';
 		str = ft_strjoin(str, buffer);
-		// printf("\nstr: %s\n", str);
 		if ((i = index_caract(str, '\n')) != -1)
-		{
-			// // printf("\nEntrou no If dentro do While\n");
 			return (get_line(&str, line, i));
-		}
 	}
-	// // printf("\nSaiu do While");
 	if (str)
 	{
-		// // printf("\nEntrou no If do free\n");
 		*line = ft_strdup(str);
 		free(str);
 		str = NULL;
 	}
+	*line = ft_strdup("");
 	return (ret);
-}
-
-int main(void)
-{
-	char *line;
-	int fd;
-	int end = 5;
-
-	fd = 0;
-	// fd = open("file", O_RDONLY);
-	while (end--)
-	{
-		get_next_line(fd, &line);
-		printf("%s\n", line);
-	}
-
 }
